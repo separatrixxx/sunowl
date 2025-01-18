1f6ba0163cc46e899daf34e7d0b2a0f7bef26eac
separatrixxx <nikitalochmatov@gmail.com>
2025-01-18 14:57:34 +0300

    fix css

--- components/ProfileComponents/ConnectList/ConnectList.tsx
+++ components/ProfileComponents/ConnectList/ConnectList.tsx
@@ -8,11 +8,11 @@ export const ConnectList = (): JSX.Element => {
 
     return (
         <>
-            {user.data.authentication.slice(3, user.data.authentication.length).map(cb => {
+            {user.data.authentication.slice(3, user.data.authentication.length).map((cb, i) => {
                 const type = Object.keys(cb).find(key => key !== 'description' && key !== 'auth_url');
 
                 return (
-                    <ConnectItem type={type || 'ton_wallet'}
+                    <ConnectItem key={i} type={type || 'ton_wallet'}
                         title={setLocale(tgUser?.language_code)['connect_' + type as 'connect_ton_wallet']}
                         text={setLocale(tgUser?.language_code)['you_will_need_it_' + type as 'you_will_need_it_ton_wallet']}
                         isConnected={user.status === 'success' ? !Boolean(cb.auth_url) : false}
